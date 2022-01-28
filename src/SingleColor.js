636463efc1b0979805123b1f1ef50f0addf000c5
jaffasunny <jaffer.sunny125@gmail.com>
2022-01-28 18:05:23 +0500

    Completed

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
+// import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, hex, index }) => {
 	const [alert, setAlert] = useState(false);
 	const bcg = rgb.join(",");
 	const hexValue = `#${hex}`;
 
-	const handleClick = (e) => {
-		setAlert(true);
-		navigator.clipboard.writeText(hexValue);
-		setTimeout(() => {
+	useEffect(() => {
+		const timeout = setTimeout(() => {
 			setAlert(false);
-		}, 3000);
-	};
+		}, 2000);
+
+		return () => clearTimeout(timeout);
+	}, [alert]);
 
 	return (
 		<article
-			onClick={handleClick}
+			onClick={() => {
+				setAlert(true);
+				navigator.clipboard.writeText(hexValue);
+			}}
 			className={`color ${index > 10 && "color-light"}`}
 			style={{ backgroundColor: `rgb(${bcg})` }}>
 			<p className="percent-value">{weight}%</p>
-			<p className="percent-color">{hexValue}</p>
+			<p className="color-value">{hexValue}</p>
 			{alert && <p className="alert">copied to the clipboard</p>}
 		</article>
 	);
